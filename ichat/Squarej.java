93e4c6550912cc59e491dfc415fe5442ea8d4ec1
Surya Yadav <157280253+SuryaYadav731@users.noreply.github.com>
2024-10-04 19:17:11 +0530

    Add files via upload

---
+++ ichat/Squarej.java
@@ -0,0 +1,16 @@
+package com.ichat;
+
+public class Squarej {
+
+        public static void main(String[] args) {
+            int[] nu= {2, 3, 4};
+            for (int n : nu) {
+                int squ = n * n;
+                int cub = n * n * n;
+                System.out.println(squ);
+                System.out.println(cub);
+            }
+        }
+    }
+
+
